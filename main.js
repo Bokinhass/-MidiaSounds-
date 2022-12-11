09cdd6633748971e0d8c9705c41b1f5f22901a04
Bokinhass <pedrohenrique_maciel@hotmail.com>
2022-12-11 12:23:44 -0300

    no message

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom (selectorAudio) {
+    const elemento = document.querySelector(selectorAudio).play();
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento ou seletor inválido!')
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i=0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
